b19d2e9ffe0ff0832d2e221055ee3f74b7d74aa4
Saber Soleymani <postorder@gmail.com>
2019-09-05 10:46:38 -0400

    weblogos added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import './App.css';
 import MuiTreeView from 'material-ui-treeview';
+import classification from './data/classification.json';
 
-const tree = [
-  {
-    value: 'Parent A',
-    nodes: [{ value: 'Child A' }, { value: 'Child B' }],
-  },
-  {
-    value: 'Parent B',
-    nodes: [
-      {
-        value: 'Child C',
-      },
-      {
-        value: 'Parent C',
-        nodes: [
-          { value: 'Child D' },
-          { value: 'Child E' },
-          { value: 'Child F' },
-        ],
-      },
-    ],
-  },
-];
+let tree=[];
 
-function App() {
+function convertToTree(data)
+{
+  for (let i =0; i < data.length;i++)
+  {
+    console.log(classification[i].Group)
+
+  }
+  return data;
+}
+tree = convertToTree(classification);
+console.log(tree);
+
+function App() 
   return (<MuiTreeView tree={tree} onLeafClick={node => alert(`${node} clicked`)} />);
 }
 
