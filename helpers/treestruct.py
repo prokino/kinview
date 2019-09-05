230ac6677afdc3e3070a20ab4766cc0e73acb470
noghte <postorder@gmail.com>
2019-09-05 16:47:42 -0400

    treestruct package added

---
+++ helpers/treestruct.py
@@ -0,0 +1,118 @@
+import uuid
+
+def sanitize_id(id):
+    return id.strip().replace(" ", "")
+
+(_ADD, _DELETE, _INSERT) = range(3)
+(_ROOT, _DEPTH, _WIDTH) = range(3)
+
+class Node:
+
+    def __init__(self, name, identifier=None, expanded=True):
+        self.__identifier = (str(uuid.uuid1()) if identifier is None else
+                sanitize_id(str(identifier)))
+        self.name = name
+        self.expanded = expanded
+        self.__bpointer = None
+        self.__fpointer = []
+
+    @property
+    def identifier(self):
+        return self.__identifier
+
+    @property
+    def bpointer(self):
+        return self.__bpointer
+
+    @bpointer.setter
+    def bpointer(self, value):
+        if value is not None:
+            self.__bpointer = sanitize_id(value)
+
+    @property
+    def fpointer(self):
+        return self.__fpointer
+
+    def update_fpointer(self, identifier, mode=_ADD):
+        if mode is _ADD:
+            self.__fpointer.append(sanitize_id(identifier))
+        elif mode is _DELETE:
+            self.__fpointer.remove(sanitize_id(identifier))
+        elif mode is _INSERT:
+            self.__fpointer = [sanitize_id(identifier)]
+
+class Tree:
+
+    def __init__(self):
+        self.nodes = []
+
+    def get_index(self, position):
+        for index, node in enumerate(self.nodes):
+            if node.identifier == position:
+                return index
+        return -1
+
+    def create_node(self, name, identifier=None, parent=None):
+        #if added by saber
+        if (identifier is None):
+            identifier = name
+            
+        node = Node(name, identifier)
+        self.nodes.append(node)
+        self.__update_fpointer(parent, node.identifier, _ADD)
+        node.bpointer = parent
+        return node
+
+    def show(self, position, level=_ROOT):
+        queue = self[position].fpointer
+        if level == _ROOT:
+            print("{0} [{1}]".format(self[position].name,
+                                     self[position].identifier))
+        else:
+            print("\t"*level, "{0} [{1}]".format(self[position].name,
+                                                 self[position].identifier))
+        if self[position].expanded:
+            level += 1
+            for element in queue:
+                self.show(element, level)  # recursive call
+
+    def expand_tree(self, position, mode=_DEPTH):
+        # Python generator. Loosly based on an algorithm from 'Essential LISP' by
+        # John R. Anderson, Albert T. Corbett, and Brian J. Reiser, page 239-241
+        yield position
+        queue = self[position].fpointer
+        while queue:
+            yield queue[0]
+            expansion = self[queue[0]].fpointer
+            if mode is _DEPTH:
+                queue = expansion + queue[1:]  # depth-first
+            elif mode is _WIDTH:
+                queue = queue[1:] + expansion  # width-first
+
+    def is_branch(self, position):
+        return self[position].fpointer
+
+    def __update_fpointer(self, position, identifier, mode):
+        if position is None:
+            return
+        else:
+            self[position].update_fpointer(identifier, mode)
+
+    def __update_bpointer(self, position, identifier):
+        self[position].bpointer = identifier
+
+    def __getitem__(self, key):
+        index = self.get_index(key)
+        if index <0:
+            return None
+        return self.nodes[index]
+
+    def __setitem__(self, key, item):
+        self.nodes[self.get_index(key)] = item
+
+    def __len__(self):
+        return len(self.nodes)
+
+    def __contains__(self, identifier):
+        return [node.identifier for node in self.nodes
+                if node.identifier is identifier]
