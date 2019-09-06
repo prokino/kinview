87b5432464139e8bc4587ffa8aa05ca76b9e6b2a
noghte <postorder@gmail.com>
2019-09-06 14:01:52 -0400

    dev

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import tree from './data/classification.json';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -20,46 +25,63 @@ const useStyles = makeStyles(theme => ({
 // console.log(tree);
 
 function App() {
+  const [value, setValue] = React.useState('rdbfirst');
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+  function treeClicked(node)
+  {
+    let path =[]
+    path.push(node.value);
+    while (node!=null)
+    {
+      if (node.parent!=null)
+        path.push(node.parent.value);
+      node = node.parent;
+    }
+    console.log(path);
+    //node => alert(`${node} clicked`)
+
+  }
+
   const classes = useStyles();
   return (
     <div className={classes.root}>
-    <Grid container spacing={3}>
-      <Grid>
-        <Paper className={classes.paper}>Kinview</Paper>
-      </Grid>
-      <Grid width={1/8}>
-        <Paper className={classes.paper}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>KinView</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>
           <Box>
-            <MuiTreeView tree={tree} onLeafClick={node => alert(`${node} clicked`)} />
-         </Box>
-        </Paper>
+          <FormControl component="fieldset">
+      {/* <FormLabel component="legend">labelPlacement</FormLabel> */}
+          <RadioGroup aria-label="position" name="position" value={value} onChange={handleChange} row>
+              <FormControlLabel
+                value="rdbfirst"
+                control={<Radio color="primary" />}
+                label="First"
+                labelPlacement="start"
+              />
+              
+              <FormControlLabel
+                value="rdbsecond"
+                control={<Radio color="primary" />}
+                label="Second"
+                labelPlacement="start"
+              />
+          </RadioGroup>
+          </FormControl>
+            <MuiTreeView tree={tree} onParentClick={treeClicked} onLeafClick={treeClicked} />
+          </Box>  
+          </Paper>
+        </Grid>
+        <Grid item xs={10}>
+          <Paper className={classes.paper}>weblogo</Paper>
+        </Grid>
       </Grid>
-      <Grid width={7/8}>
-        <Paper className={classes.paper}>ss</Paper>
-      </Grid>
-      {/* <Grid item xs={6} sm={3}>
-        <Paper className={classes.paper}>xs=6 sm=3</Paper>
-      </Grid>
-      <Grid item xs={6} sm={3}>
-        <Paper className={classes.paper}>xs=6 sm=3</Paper>
-      </Grid>
-      <Grid item xs={6} sm={3}>
-        <Paper className={classes.paper}>xs=6 sm=3</Paper>
-      </Grid>
-      <Grid item xs={6} sm={3}>
-        <Paper className={classes.paper}>xs=6 sm=3</Paper>
-      </Grid> */}
-    </Grid>
-  </div>
-  //   <React.Fragment>
-  //   <Container>
-  //       <Box width={1/6}>
-  //         <MuiTreeView tree={tree} onLeafClick={node => alert(`${node} clicked`)} />
-  //     </Box>
-      
-  //   </Container>
-  // </React.Fragment>
-
+    </div>
   );
 }
 
