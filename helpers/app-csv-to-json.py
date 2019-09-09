675c3a76223184dc2a5eccbd6ae02a08b1421dfd
Saber Soleymani <postorder@gmail.com>
2019-09-09 02:34:27 -0400

    basics are ready

--- helpers/app-csv-to-json.py
+++ helpers/app-csv-to-json.py
@@ -14,7 +14,7 @@ def main():
         for row in csvreader:
             group = row['Group']
             if not any(g['value'] == group for g in groups): #if group not already added to the groups
-                groups.append({'value':group, 'nodes': []})
+                groups.append({'value':group, 'HasAlignment': row['HasAlignment'], 'nodes': []})
             
         #data = groups
     
@@ -25,7 +25,7 @@ def main():
             family = row['Family']
             if family != '': #and not family in group.nodes:
                 group = next(x for x in groups if x['value'] == row['Group']) #find the first (and the only) group having the group name
-                entity = {'value':family, 'nodes': []}
+                entity = {'value':family, 'HasAlignment': row['HasAlignment'],'nodes': []}
                 if not entity in group['nodes']:
                     group['nodes'].append(entity)
                 
@@ -37,7 +37,9 @@ def main():
             if subfamily != '':
                 group = next(x for x in groups if x['value'] == row['Group'])
                 family = next(x for x in group['nodes'] if x['value'] == row['Family'])
-                family['nodes'].append({'value':subfamily,'path':"{0}/{1}/{2}".format(group['value'],family['value'],subfamily), 'nodes': []})
+                family['nodes'].append({'value':subfamily,
+                                        'path':"{0}_{1}_{2}".format(group['value'],family['value'],subfamily),
+                                        'HasAlignment': row['HasAlignment'] })
     
     with open(jsonPath, 'w') as f:
         f.write(json.dumps(groups, indent=4))
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import MuiTreeView from 'material-ui-treeview';
+//import MuiTreeView from 'material-ui-treeview';
+import MuiTreeView from './MuiTreeView';
 import tree from './data/classification.json';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
@@ -9,7 +10,7 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
-import { Typography } from '@material-ui/core';
+import { Typography, Drawer } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -34,25 +35,60 @@ function App() {
   function handleChange(event) {
     setRdbValue(event.target.value);
   }
-  function leafClicked(node)
+  function setPath(path)
   {
-    const path = node.path;//.split("/");
-    console.log(path);
     if (rdbvalue === 'rdbfirst')
       setFirstLabel(path);
     else if (rdbvalue === 'rdbsecond')
       setSecondLabel(path);
   }
+  function leafClicked(node)
+  {
+    const path = node.path;//leaves have node path
+    console.log(path);
+    setPath(path);
+  }
   function nodeClicked(node)
   {
-    let path =[]
-    
+    let path =[] //non-leaves don't have path, so we should build one
     if (node.parent!=null)
       path.push(node.parent.value);
     path.push(node.value);
-    console.log(path);
+    
+    setPath(path);
     //node => alert(`${node} clicked`)
 
+  }
+  function getImage(lbl)
+  {
+    console.log(lbl)
+    let src = '';
+    if (lbl !== '')
+      src = 'weblogo/family/ePKf_' + lbl + '.png';
+    
+    return src;
+    
+    //return 'weblogo/family/ePKf_TKL_TKL-Unique.png';
+  }
+  function drawd()
+  {
+    var img = document.getElementById("firstImage");
+    if (img!=null && img.src!='')
+    {  
+     
+      var cnvs = document.getElementById("firstCanvas");
+     console.log(cnvs);
+      cnvs.style.position = "absolute";
+      cnvs.style.left = img.offsetLeft + "px";
+      cnvs.style.top = img.offsetTop + "px";
+      
+      var ctx = cnvs.getContext("2d");
+      ctx.beginPath();
+      ctx.arc(250, 210, 200, 0, 2 * Math.PI, false);
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = '#00ff00';
+      ctx.stroke();
+    }
   }
 
   const classes = useStyles();
@@ -88,10 +124,16 @@ function App() {
           </Paper>
         </Grid>
         <Grid item xs={10}>
-          <Typography variant="h6" id="modal-title">{firstLabel}</Typography>
-          <Paper className={classes.paper}>weblogo first</Paper>
-          <Typography variant="h6" id="modal-title">{secondLabel}</Typography>
-          <Paper className={classes.paper}>weblogo second</Paper>
+          <Paper className={classes.paper}>
+            <Typography variant="h6" id="modal-title">{firstLabel}</Typography>
+            {/* <canvas id='firstCanvas'></canvas> */}
+            <img id='firstImage' src={getImage(firstLabel)} />
+          </Paper>
+          
+          <Paper className={classes.paper}>
+            <Typography variant="h6" id="modal-title">{secondLabel}</Typography>
+            <img id='secondImage' src={getImage(secondLabel)} />
+          </Paper>
         </Grid>
       </Grid>
     </div>
