9f347ce7301b85b037c0889f92a009c085f262a0
noghte <postorder@gmail.com>
2019-09-09 20:04:12 -0400

    dev

--- helpers/app-csv-to-json.py
+++ helpers/app-csv-to-json.py
@@ -1,6 +1,7 @@
 import csv, json
 #from treestruct import Tree
-
+def getvalue(s):
+    return s.split('@')[1]
 def main():
     csvPath = 'src/data/classification_july1_hasAln.csv'
     jsonPath = 'src/data/classification.json'
@@ -10,34 +11,40 @@ def main():
     #Group
     with open(csvPath) as f:
         csvreader = csv.DictReader(f)
-        
+        idx = 0
         for row in csvreader:
+            idx += 1
             group = row['Group']
             if not any(g['value'] == group for g in groups): #if group not already added to the groups
-                groups.append({'value':group, 'HasAlignment': row['HasAlignment'], 'nodes': []})
+                groups.append({'id': "id" + str(idx) + "@" + group, 'value':group, 'HasAlignment': row['HasAlignment'], 'nodes': []})
             
         #data = groups
     
     #Family
     with open(csvPath) as f:
         csvreader = csv.DictReader(f)
+        idx = 0
         for row in csvreader:
+            idx += 1
             family = row['Family']
             if family != '': #and not family in group.nodes:
                 group = next(x for x in groups if x['value'] == row['Group']) #find the first (and the only) group having the group name
-                entity = {'value':family, 'HasAlignment': row['HasAlignment'],'nodes': []}
-                if not entity in group['nodes']:
+                if not any(x for x in group['nodes'] if x['value'] == family): #in group['nodes']['text']:
+                    entity = {'id':"id" + str(idx) + "@" + family, 'value':family, 'HasAlignment': row['HasAlignment'],'nodes': []}
                     group['nodes'].append(entity)
                 
     #Subfamily
     with open(csvPath) as f:
         csvreader = csv.DictReader(f)
+        idx = 0
         for row in csvreader:
+            idx += 1
             subfamily = row['Subfamily']
             if subfamily != '':
                 group = next(x for x in groups if x['value'] == row['Group'])
                 family = next(x for x in group['nodes'] if x['value'] == row['Family'])
-                family['nodes'].append({'value':subfamily,
+                family['nodes'].append({'id':"id" + str(idx) + "@" + subfamily,
+                                        'value': subfamily,
                                         'path':"{0}_{1}_{2}".format(group['value'],family['value'],subfamily),
                                         'HasAlignment': row['HasAlignment'] })
     
--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import { Typography, Drawer } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import * as d3 from "d3";
 
+const rowWidth = 30, rowHeight = 120;
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
@@ -24,23 +26,25 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 // console.log(tree);
+const imgLogoStyle = {
+  width: '100px'
+};
 
 function App() {
-  
-
   const [rdbvalue, setRdbValue] = React.useState('rdbfirst');
   const [firstLabel, setFirstLabel] = React.useState('');
   const [secondLabel, setSecondLabel] = React.useState('');
-
+  const [structureDrew,setStructureDrew] = React.useState(false)
   function handleChange(event) {
     setRdbValue(event.target.value);
   }
   function setPath(path)
   {
-    if (rdbvalue === 'rdbfirst')
-      setFirstLabel(path);
-    else if (rdbvalue === 'rdbsecond')
-      setSecondLabel(path);
+    rdbvalue === 'rdbfirst' ? setFirstLabel(path) : setSecondLabel(path);
+    // if (rdbvalue === 'rdbfirst')
+    //   setFirstLabel(path);
+    // else if (rdbvalue === 'rdbsecond')
+    //   setSecondLabel(path);
   }
   function leafClicked(node)
   {
@@ -73,7 +77,7 @@ function App() {
   function drawd()
   {
     var img = document.getElementById("firstImage");
-    if (img!=null && img.src!='')
+    if (img!==null && img.src!=='')
     {  
      
       var cnvs = document.getElementById("firstCanvas");
@@ -90,13 +94,116 @@ function App() {
       ctx.stroke();
     }
   }
-
+  function draw_sstructure() {
+    //setStructureDrew()
+    if (!structureDrew)
+    {
+    var betas_start = [1,14,25,60,69,115,125,133,142], betas_end = [9,20,33,65,75,116,127,135,143], helix_start = [39,82,93,173,198,212,233], helix_end = [51,89,112,188,207,221,236], betas = [1,2,3,4,5,6,7,8,9], alphas = ['C','D','E','F','G','H','I'];
+        //generate svg window	
+        var sstruct = d3.select("#sstruct")
+          .append("svg")
+          .attr("width",242*rowWidth)
+          .attr("height",70);
+        //loops
+        sstruct.append("rect")
+          .attr("width", rowWidth*242)
+          .attr("height", "2")
+          .attr("x", "0")
+          .attr("y", "20")
+          //.attr("x", function (d) { return this.parentNode.getBBox().x; })
+          //.attr("y", function (d) { return this.parentNode.getBBox().y; })
+          .attr("stroke", "black")
+          .attr("stroke-width",2);
+  
+        //Separate each beta strand/helix and apply mouseover labels
+        for (let i=0; i<betas_start.length; i+=1) {
+          sstruct.append("rect")
+            .attr("width", function (d) { return (betas_end[i]-betas_start[i])*rowWidth} )
+            .attr("height", "4")
+            .attr("x", function (d) { return (betas_start[i]-1)*rowWidth; })
+            .attr("y", "19")
+            .attr("stroke", "blue")
+            .attr("fill", "blue")
+            .attr("stroke-width","10");
+          sstruct.append("text")
+            .attr("font-family", "monospace")
+            .attr("font-size", "12pt")
+            .attr("stroke", "blue")
+            .attr("fill", "blue")
+            .attr("transform", function (d) { return "translate("+(betas_start[i]-1+0.5*(betas_end[i]-betas_start[i]))*rowWidth+",45)"})
+            .text('\u03B2'+betas[i]);
+        }
+  
+        for (let i=0; i<betas_end.length; i+=1) {
+          sstruct.append("path")
+            .attr("d", function (d) { var temp = (betas_end[i]-1)*rowWidth;
+                    return "M "+(temp+10)+" 21 L "+temp+" 32 L "+temp+" 10 L "+(temp+10)+" 21"})
+            //.attr("d", "M 20 61 L 0 75 L 0 48 L 20 61")
+            .attr("stroke", "blue")
+            .attr("fill", "blue")
+            .attr("stroke-width",3);
+        }
+  
+        for (let i=0; i<helix_start.length; i+=1) {
+          sstruct.append("rect")
+            .attr("width", function (d) { return (helix_end[i]-helix_start[i]+1)*rowWidth} )
+            .attr("height", "20")
+            .attr("x", function (d) { return helix_start[i]*rowWidth; })
+            .attr("y", "12")
+            //.attr("height", function (d) { return this.parentNode.getBBox().height+18; })
+            //.attr("x", function (d) { return this.parentNode.getBBox().x; })
+            //.attr("y", function (d) { return this.parentNode.getBBox().y+50; })
+            .attr("stroke", "green")
+            .attr("fill", "green")
+            .attr("stroke-width",3);
+          sstruct.append("text")
+            .attr("font-family", "monospace")
+            .attr("font-size", "12pt")
+            .attr("stroke", "green")
+            .attr("fill", "green")
+            .attr("transform", function (d) { return "translate("+(helix_start[i]+0.5*(helix_end[i]-helix_start[i]))*rowWidth+",45)"})
+            .text('\u03B1'+alphas[i]);
+  
+        }
+        setStructureDrew(true);
+      }
+        
+  };
+function annotations()
+{
+  let pdata = [];
+        for (let i=1; i<=241; i+=1) {
+          pdata.push({'pos':i,
+              'x':rowWidth*(i-1),
+              'y':120});
+        }
+        const locale = "firstImage"
+        const vis = d3.selectAll("#sequences #"+locale);
+        const letterbox = vis.selectAll("png")
+        .data(pdata)
+        .enter()
+        .append("g")
+        .attr("transform", function(d) { return "translate("+(d.x+5)+","+d.y+")"; })
+        .attr("id", function (d) { return "p"+d.pos; });
+      console.log(vis);
+      letterbox.append("g")
+        .attr("id", "data-seq")
+        .attr("transform", "translate(3,-10)");
+      letterbox.append("g")
+        .attr("transform", "translate(8,-10)")
+        .attr("id", "data-num");
+      letterbox.append("g")
+        .attr("id", "data-ffeats");
+  
+}
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>KinView</Paper>
+          <Paper className={classes.paper}>
+            <img id="logo" src='img/kinview-logo.png' style={imgLogoStyle} />
+          </Paper>
         </Grid>
         <Grid item xs={2}>
           <Paper className={classes.paper}>
@@ -123,13 +230,15 @@ function App() {
           </Box>  
           </Paper>
         </Grid>
-        <Grid item xs={10}>
+        <Grid id="sequences" item xs={10}>
           <Paper className={classes.paper}>
             <Typography variant="h6" id="modal-title">{firstLabel}</Typography>
             {/* <canvas id='firstCanvas'></canvas> */}
             <img id='firstImage' src={getImage(firstLabel)} />
           </Paper>
-          
+          {draw_sstructure()}
+          {annotations()}
+          <div id="sstruct"></div>
           <Paper className={classes.paper}>
             <Typography variant="h6" id="modal-title">{secondLabel}</Typography>
             <img id='secondImage' src={getImage(secondLabel)} />
