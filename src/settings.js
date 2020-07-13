91b89a29e60c83d090d528bcfa5e16269a94bf23
saber <postorder@gmail.com>
2020-07-12 22:39:55 -0400

    settings file added

---
+++ src/settings.js
@@ -0,0 +1,7 @@
+export const settings = {
+    treeview_only_dark: ["kinase"],
+    ptmb_checkbox:["kinase"],
+    weblogo_div:["kinase"],
+    aligend_seq_dropdown:["kinase","gta"],
+    ortholog_seq_dropdown:["kinase","gta"]
+};
